85ebbf167914e9fe69b9446b36760be93906c688
Martin Gomez <jampo1234@gmail.com>
2018-09-11 18:18:46 -0300

    Mapa de Prueba terminado

---
+++ Sprites Tiled/Agua AO.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ImpAO_Foto11 (2)" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="../Sprites/Pasto/ImpAO_Foto11 (2).jpg" width="1024" height="768"/>
+</tileset>
